b0498bca5f64e18f3987a1cf09c1952c9cf6e249
WolfSideChan <bernardomrtns2002@hotmail.com>
2024-03-29 18:46:51 +0200

    Plugin Atualizado

---
+++ src/main/java/pt/bernardo/managers/PlayerBlockManagers.java
@@ -0,0 +1,45 @@
+package pt.bernardo.managers;
+
+import pt.bernardo.objetos.PlayerBlock;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.Arrays;
+import java.util.List;
+
+import static pt.bernardo.Main.*;
+
+public class PlayerBlockManagers {
+
+    public static void insert() {
+
+        PlayerBlock joao = new PlayerBlock("joao", 5);
+        PlayerBlock carlos = new PlayerBlock("carlos", 20);
+
+        List<PlayerBlock> list = Arrays.asList(joao, carlos);
+
+        try {
+
+            getMySQL().openConnection();
+            Connection connection = getMySQL().getConnection();
+
+            PreparedStatement ps = null;
+
+
+            for (PlayerBlock pb: list) {
+                ps = connection.prepareStatement("INSERT INTO informacoes VALUES ('"+pb.getNome()+"',''"+pb.getQuantidade()+")");
+                ps.execute();
+            }
+            ps.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            getMySQL().closeConnection();
+        }
+
+
+
+    }
+
+}
---
+++ src/main/java/pt/bernardo/objetos/PlayerBlock.java
@@ -0,0 +1,27 @@
+package pt.bernardo.objetos;
+
+public class PlayerBlock {
+
+    private String nome;
+    private int quantidade;
+
+    public PlayerBlock(String nome, int quantidade) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+}
